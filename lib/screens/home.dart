18d45d8583b984a8006d551a6f1ccab68cceef69
Dennis Moes <dennismoes@me.com>
2024-05-27 08:57:06 +0200

    Changed a small part of the layout

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -11,32 +11,43 @@ class HomeScreen extends StatelessWidget {
         title: const Text("Home screen"),
       ),
       body: Center(
-        child: ElevatedButton(
-          child: const Text("Press me"),
-          onPressed: () {
-            Navigator.push(
-              context,
-              PageRouteBuilder(
-                pageBuilder: (_, __, ___) => const DetailScreen(),
-                transitionDuration: const Duration(milliseconds: 350),
-                fullscreenDialog: true,
-                transitionsBuilder:
-                    (context, animation, secondaryAnimation, child) {
-                  var begin = const Offset(0.0, 1.0);
-                  var end = Offset.zero;
-                  var curve = Curves.easeOutCubic;
-
-                  var tween = Tween(begin: begin, end: end)
-                      .chain(CurveTween(curve: curve));
-
-                  return SlideTransition(
-                    position: animation.drive(tween),
-                    child: child,
-                  );
-                },
+        child: Column(
+          children: [
+            const Hero(
+              tag: 'HouseImg',
+              child: Image(
+                image: AssetImage('assets/images/house_img.jpg'),
+                width: 80,
               ),
-            );
-          },
+            ),
+            ElevatedButton(
+              child: const Text("Press me"),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  PageRouteBuilder(
+                    pageBuilder: (_, __, ___) => const DetailScreen(),
+                    transitionDuration: const Duration(milliseconds: 350),
+                    fullscreenDialog: true,
+                    transitionsBuilder:
+                        (context, animation, secondaryAnimation, child) {
+                      var begin = const Offset(0.0, 1.0);
+                      var end = Offset.zero;
+                      var curve = Curves.easeOutCubic;
+
+                      var tween = Tween(begin: begin, end: end)
+                          .chain(CurveTween(curve: curve));
+
+                      return SlideTransition(
+                        position: animation.drive(tween),
+                        child: child,
+                      );
+                    },
+                  ),
+                );
+              },
+            ),
+          ],
         ),
       ),
     );
